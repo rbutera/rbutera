2bccf13bfcfea889f1fb09ef8677c7f68be962e4
Rai <rai@rbutera.com>
2017-11-19 20:23:09 +0000

    added tar gz generation to build

--- client/webpack.prod.js
+++ client/webpack.prod.js
@@ -1,12 +1,19 @@
 const merge = require("webpack-merge");
 const common = require("./webpack.common.js");
 const UglifyJSPlugin = require("uglifyjs-webpack-plugin");
+const WebpackShellPlugin = require("webpack-shell-plugin");
+const PACKAGE = require("./package.json");
+const PACKAGE_VERSION = PACKAGE.version;
 
 module.exports = merge(common, {
   devtool: "source-map",
   plugins: [
     new UglifyJSPlugin({
       sourceMap: true
+    }),
+    new WebpackShellPlugin({
+      onBuildStart: ['rm -rfv rbutera*.tar.gz"'],
+      onBuildEnd: ["tar -zcvf rbutera-v" + PACKAGE_VERSION + ".tar.gz dist"]
     })
   ]
 });
